4fd24c5b706b75dcd1505d515ddc086f5217811e
fomad <thefomadd@gmail.com>
2016-06-16 09:09:10 +0300

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,13 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 FlightBooker::Application.initialize!
 
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :mail => ENV['SENDGRID_MAIL'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
